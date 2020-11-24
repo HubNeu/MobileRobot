1441cd4ea5115f6156a8e6a8a130a3d2ea447334
BolleyVall7 <oskarzborowski@gmail.com>
2020-11-24 16:39:24 +0100

    template

---
+++ models/start_model.php
@@ -0,0 +1,14 @@
+<?php
+
+class Start_model extends Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function sampleMethod()
+    {
+        return 'sampleMethod';
+    }
+}
---
+++ views/common/pageNotFound.php
@@ -0,0 +1,3 @@
+<pre>Error <?= $this->body['externalError'][0] ?></pre>
+<pre><?= $this->body['errorMessage'][0] ?></pre>
+<pre><?= $this->body['internalError'][0] ?></pre>
