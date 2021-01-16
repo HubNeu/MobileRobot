871cdf5b3f068c996191cf46f0440b1d603e6105
HubNeu <49408414+HubNeu@users.noreply.github.com>
2021-01-16 20:30:25 +0100

    Delete main.php

--- views/start/main.php
+++
@@ -1,7 +0,0 @@
-<div id="main">
-    <img id="image" src="<?= $path ?>img/politechnika.jpg">
-</div>
-<div id="nav">
-    <button id="play" class="nav" onclick="play()"><img class="icon" src="<?= $path ?>img/play.png"></button>
-    <button id="stop" class="nav" onclick="stop()" disabled><img class="icon" src="<?= $path ?>img/stop.png"></button>
-</div>
